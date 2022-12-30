b345c82b76f173c2526ae84d98987f51609646b8
eonias189 <eonias189@yandex.ru>
2022-12-30 21:03:40 +0400

    доделано start_cutscene.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-pass
+import pygame
+from tools import load_image, terminate
+from start_cutscene import start_cutscene
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 1600, 900
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Scary smile')
+    FPS = 60
+    start_cutscene()
